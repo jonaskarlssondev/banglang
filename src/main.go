9135bbfa429b8e49599e43818e0ecb425f3405bf
Jonas Karlsson <jonas@jonaskarlsson.dev>
2024-08-27 23:53:05 +0200

    add repl and repl runner

--- src/main.go
+++ src/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"banglang/repl"
+	"fmt"
+	"os"
+	"os/user"
+)
 
+func main() {
+	usr, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Bang language!\n", usr.Username)
+	fmt.Printf("Feel free to type in commands\n")
+
+	repl.Start(os.Stdin, os.Stdout)
 }
